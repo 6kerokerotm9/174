728b458541afd6958dfde925094aa2310f273bdf
Joshua <luckystripes60@gmail.com>
2020-02-16 19:39:24 -0800

    submission

--- hw1/primes.php
+++ hw1/primes.php
@@ -1,4 +1,11 @@
 <?php
+    /*
+    Joshua Sjah
+    CS 174
+    This program describes two functions, a function that finds primes up to the input and 
+    a second function that tests the first by printing the output and the expected. 
+    */
+
     function prime($input) {
         $startOfPrimes = 2; //primes start at 2 so start testing from the number 2
         for($i=$startOfPrimes; $i<$input; $i++) { 
@@ -34,6 +41,11 @@
         echo "<br>output: ";
         prime(69);
         echo "expected: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67<br>";
+
+        echo "<br>output: ";
+        prime(100);
+        echo "expected: 2, 3, 5, 7, 11, 13, 17,
+              19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97<br>";
     } 
 
     tester();
